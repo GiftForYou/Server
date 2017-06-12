bef08f28239e333f32b9eff2a1a48e331c12c8c3
zanisis <adzannyaa@gmail.com>
2017-06-12 10:37:06 +0700

    modified index.js

---
+++ controllers/recomendation.js
@@ -0,0 +1,10 @@
+
+
+let recomendation = {}
+
+recomendation.searchs = (req, res, next)=>{
+  console.log('kesini gak');
+  console.log(req.body.recomendation)
+}
+
+module.exports = recomendation;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const controllers = require('../controllers/birthdays');
+const recomendations = require('../controllers/recomendation')
 
 /* GET home page. */
 router.get('/birthday', controllers.getAll);
 
 router.post('/birthday', controllers.createBirth)
 router.delete('/birthday/:id', controllers.deleteBirth)
+
+router.post('/recomendation', recomendations.searchs)
 
 module.exports = router;
