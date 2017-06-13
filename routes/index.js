dae000a61bb96eb842ee718aba7c9f9aeaa6c1c0
fearzen3 <ivanhabi2@gmail.com>
2017-06-13 10:03:19 +0700

    update server

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 var express = require("express");
 var router = express.Router();
-const controllers = require("../controllers/birthdays");
-const cheerio = require("../controllers/scrape");
 const analyze = require("../controllers/analyzeData");
 const bklpk = require("../controllers/bukaLapak");
 /* GET home page. */
-router.get("/birthday", controllers.getAll);
-
-router.post("/birthday", controllers.createBirth);
-router.delete("/birthday/:id", controllers.deleteBirth);
-
-router.get("/scrape", cheerio);
-
 router.post("/recomendation", analyze.giftRecomendation, bklpk.reqGift);
 
 module.exports = router;
