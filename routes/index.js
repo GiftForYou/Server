706260f10b36ae8dd72ca54e9ad5ebe73b00ffc9
fearzen3 <ivanhabi2@gmail.com>
2017-06-13 12:15:51 +0700

    dikitlagi

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,5 @@ const analyze = require("../controllers/analyzeData");
 const bklpk = require("../controllers/bukaLapak");
 /* GET home page. */
 router.post("/recomendation", analyze.giftRecomendation, bklpk.reqGift);
-// router.post("/recomendation", bklpk.reqGift);
-// router.get("/recomendation", bklpk.getGift);
-
-// router.post('/recomendation', recomendations.searchs)
 
 module.exports = router;
